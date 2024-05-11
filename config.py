4afb44d35c60f647e0b16fc1b4ab1c9dd286d6e1
aryanchy449 <108731844+aryanchy449@users.noreply.github.com>
2024-05-11 11:08:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ import os
 
 class Config(object):
     # Mandotory
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    APP_ID = int(os.environ.get("23080322"))
+    API_HASH = os.environ.get("b3611c291bf82d917637d61e4a136535")
+    BOT_TOKEN = os.environ.get("6931048916:AAEiwn5cej-PVBw95cqsGskW-BEGx95o4jY")
+    LOGS_CHANNEL = int(os.environ.get("-1002072662567"))
+    BOT_OWNER = int(os.environ.get("6214889840"))
     MONGODB_URL = os.environ.get("MONGODB_URL")
-    GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN")
+    GOFILE_TOKEN = os.environ.get("ETgJJ7ITuYFdVBSoamumKXuzKtWuKWYb")
     # Optional
-    MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
+    MAX_DOWNLOAD_SIZE = int(os.environ.get("1024*1024*6")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
     # Constents
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/NexaBots"
     TG_MAX_SIZE = 2040108421
