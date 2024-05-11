89c92ee70dd67e371a3abe1c5fa5ae515a386d6f
aryanchy449 <108731844+aryanchy449@users.noreply.github.com>
2024-05-11 12:09:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ import os
 
 class Config(object):
     # Mandotory
-    APP_ID = int(os.environ.get("23080322"))
-    API_HASH = os.environ.get("b3611c291bf82d917637d61e4a136535")
-    BOT_TOKEN = os.environ.get("6931048916:AAEiwn5cej-PVBw95cqsGskW-BEGx95o4jY")
-    LOGS_CHANNEL = int(os.environ.get("-1002072662567"))
-    BOT_OWNER = int(os.environ.get("6214889840"))
-    MONGODB_URL = os.environ.get("mongodb+srv://hello:hello@cluster0.s7lxbia.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    GOFILE_TOKEN = os.environ.get("ETgJJ7ITuYFdVBSoamumKXuzKtWuKWYb")
+    APP_ID = int(os.environ.get("API_ID","23080322"))
+    API_HASH = os.environ.get("API_HASH","b3611c291bf82d917637d61e4a136535")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","6931048916:AAEiwn5cej-PVBw95cqsGskW-BEGx95o4jY")
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL","-1002072662567"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER","6214889840"))
+    MONGODB_URL = os.environ.get("MONGODB_URL","mongodb+srv://hello:hello@cluster0.s7lxbia.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN","ETgJJ7ITuYFdVBSoamumKXuzKtWuKWYb")
     # Optional
     MAX_DOWNLOAD_SIZE = int(os.environ.get("1024*1024*6")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
     # Constents
